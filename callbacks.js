5473702ea44370855c7ea38f19ec063a0aa81be0
Titus Lagat <lagattyto@gmail.com>
2022-10-04 11:47:29 +0300

    callbacks to deal with async code

--- callbacks.js
+++ callbacks.js
@@ -1,7 +1,12 @@
 console.log("start");
-
+//callback
 getUser(1, (user) => {
 	console.log('User:', user)
+	//callback
+	getRepositories(user.gitUsername, (repos) => {
+		console.log('Repos:', repos)
+		//another callback
+	})
 });
 
 
@@ -9,7 +14,13 @@ function getUser(id, callback) {
   setTimeout(() => {
 		console.log("Reading user from the database.....");
 		callback({ id: id, gitUsername: 'lagat'})
-  }, 3000);
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+	setTimeout(() => {
+		callback(['repo1', 'repo2', 'repo3'])
+	}, 3000)
 }
 
 console.log("end");
