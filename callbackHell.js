ebca86341a0e845b1f6bc7001fe37e6c43fc4fb4
Titus Lagat <lagattyto@gmail.com>
2022-10-04 12:05:00 +0300

    callback hell

---
+++ callbackHell.js
@@ -0,0 +1,25 @@
+//callback
+getUser(1, (user) => {
+  console.log("User:", user);
+  //callback
+  getRepositories(user.gitUsername, (repos) => {
+    console.log("Repos:", repos);
+    //another callback
+		getCommits(repo.name, (commits) => {
+			
+		})
+  });
+});
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Reading user from the database.....");
+    callback({ id: id, gitUsername: "lagat" });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    callback(["repo1", "repo2", "repo3"]);
+  }, 3000);
+}
